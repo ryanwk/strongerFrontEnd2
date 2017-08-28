e39c0c93c9ddddf4377b06dd9691a00506105f32
ryan <ryanwkoch@gmail.com>
2017-08-28 16:34:02 -0400

    changing branches
    something

--- app/models/exercise.js
+++ app/models/exercise.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   weight: DS.attr('number'),
-  updated_at: DS.attr('string'),
+  updated_at: DS.attr('date'),
 
 });
